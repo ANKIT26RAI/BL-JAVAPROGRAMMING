b537f48d21263678bc0421e9095a2a6c8f67112f
ANKIT26RAI <anky26roy@gmail.com>
2026-03-31 16:25:15 +0530

    KmtoMiles

---
+++ Java-Programming-Elements-Level1/KmtoMiles.java
@@ -0,0 +1,15 @@
+// 3. Create a program to convert the distance of 10.8 kilometers to miles.
+// Hint: 1 km = 1.6 miles
+// I/P => NONE
+// O/P => The distance ___ km in miles is ___
+
+import java.util.Scanner;
+public class KmtoMiles {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);  
+        double kilometers=10.8;
+        double miles=kilometers*1.6;
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+    
+}
