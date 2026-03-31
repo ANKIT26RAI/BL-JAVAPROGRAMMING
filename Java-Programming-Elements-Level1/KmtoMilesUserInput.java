7fbe30b1b8f4baf693f3ec8311a935c463ac15e6
ANKIT26RAI <anky26roy@gmail.com>
2026-03-31 19:06:37 +0530

    Student Fees

--- Java-Programming-Elements-Level1/KmtoMilesUserInput.java
+++ Java-Programming-Elements-Level1/KmtoMilesUserInput.java
@@ -7,15 +7,15 @@
 // d. Use 1 mile = 1.6 km formulae to calculate miles and show the output 
 // I/P => km 
 // O/P => The total miles is ___ mile for the given ___ km
-import java.util.Scanner;
-public class KmtoMilesUserInput {
-    public static void main(String[] args) {
-        double km;
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter distance in kilometers: ");
-        km = input.nextDouble();
-        double miles = km / 1.6;
-        System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
-    }
+// import java.util.Scanner;
+// public class KmtoMilesUserInput {
+//     public static void main(String[] args) {
+//         double km;
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter distance in kilometers: ");
+//         km = input.nextDouble();
+//         double miles = km / 1.6;
+//         System.out.println("The total miles is " + miles + " mile for the given " + km + " km");
+//     }
     
-}
+// }
