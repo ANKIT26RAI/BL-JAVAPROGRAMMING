c716629d7d255f7fac71a876a50754db66dc507f
ANKIT26RAI <anky26roy@gmail.com>
2026-03-31 18:58:10 +0530

    VolumeEarth

--- Java-Programming-Elements-Level1/UniversityFees.java
+++ Java-Programming-Elements-Level1/UniversityFees.java
@@ -8,14 +8,14 @@
 // d. Compute and print the fee you have to pay by subtracting the discount from the fee. 
 // O/P => The discount amount is INR ___ and final discounted fee is INR ___ 
 
-public class UniversityFees {
-    public static void main(String[] args) {
-        double fee = 125000;
-        double discountPercent = 10;
+// public class UniversityFees {
+//     public static void main(String[] args) {
+//         double fee = 125000;
+//         double discountPercent = 10;
 
-        double discount = (fee * discountPercent) / 100;
-        double discountedFee = fee - discount;
+//         double discount = (fee * discountPercent) / 100;
+//         double discountedFee = fee - discount;
 
-        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + discountedFee);
-    }
-}
+//         System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + discountedFee);
+//     }
+// }
---
+++ Java-Programming-Elements-Level1/VolumeEarth.java
@@ -0,0 +1,21 @@
+
+// Write a Program to compute the volume of Earth in km^3 and miles^3 
+// Hint => Volume of a Sphere is (4/3) * pi * r^3 and radius of earth is 6378 km 
+// O/P => The volume of earth in cubic kilometers is ____ and cubic miles is ____ 
+
+// public class VolumeEarth {
+//     public static void main(String[] args) {
+//         double radiusKm = 6378; // radius of Earth in kilometers
+//         double radiusMiles = radiusKm * 0.621371; // convert radius to miles
+
+//         // Calculate volume in cubic kilometers
+//         double volumeKm3 = (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+
+//         // Calculate volume in cubic miles
+//         double volumeMiles3 = (4.0 / 3.0) * Math.PI * Math.pow(radiusMiles, 3);
+
+//         // Print the results
+//         System.out.printf("The volume of Earth in cubic kilometers is %.2f km^3 and cubic miles is %.2f miles^3%n", volumeKm3, volumeMiles3);
+//     }
+    
+// }
