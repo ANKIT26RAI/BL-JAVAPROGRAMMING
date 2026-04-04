ca69543e9d325c9520820bd53d6b9b3341d128ce
ANKIT26RAI <anky26roy@gmail.com>
2026-04-04 23:51:31 +0530

    +,-,0

---
+++ Java-Control-Flow-Level1/Posnegzero.java
@@ -0,0 +1,25 @@
+// Write a program to check whether a number is positive, negative, or zero. 
+// Hint =>  
+// a. Get integer input from the user and store it in the number variable. 
+// b. If the number is positive, print positive. 
+// c. If the number is negative, print negative. 
+// d. If the number is zero, print zero.  
+import java.util.Scanner;
+public class Posnegzero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+
+        sc.close();
+    }
+    
+}
