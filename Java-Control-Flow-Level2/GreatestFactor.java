5d6dc40b12b76d0640b641ef600fd0e3f9729ce8
ANKIT26RAI <anky26roy@gmail.com>
2026-04-05 18:31:33 +0530

    Greatest Factor using for loop

--- Java-Control-Flow-Level2/GreatestFactor.java
+++ Java-Control-Flow-Level2/GreatestFactor.java
@@ -11,22 +11,22 @@ public class GreatestFactor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter an integer: ");
-        int number = sc.nextInt();
+        int num = sc.nextInt();
 
-        if (number <= 1) {
+        if (num <= 1) {
             System.out.println("Please enter an integer greater than 1.");
             return;
         }
 
         int greatestFactor = 1;
-        for (int i = number - 1; i >= 1; i--) {
-            if (number % i == 0) {
+        for (int i = num - 1; i >= 1; i--) {
+            if (num % i == 0) {
                 greatestFactor = i;
                 break;
             }
         }
 
-        System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+        System.out.println("The greatest factor of " + num + " besides itself is: " + greatestFactor);
     }
     
 }
