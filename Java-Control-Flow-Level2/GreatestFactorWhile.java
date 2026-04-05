7592247419775a334bf023c75c08ac4c42372982
ANKIT26RAI <anky26roy@gmail.com>
2026-04-05 18:30:09 +0530

    Greatest Factor using while

---
+++ Java-Control-Flow-Level2/GreatestFactorWhile.java
@@ -0,0 +1,36 @@
+//  Rewrite the above program to print the greatest factor of a number beside itself using a 
+// while loop. 
+// Hint =>  
+// a. Get an integer input and assign it to the number variable. As well as define a 
+// greatestFactor variable and assign it to 1 
+// b. Create a variable counter and assign counter = number - 1; Use the while loop till 
+// the counter is equal to 1. 
+// e. Inside the loop, check if the number is perfectly divisible by the counter then assign the 
+// counter to greatestFactor variable and break the loop. 
+// f. 
+// Display the greatestFactor variable outside the loop 
+import java.util.Scanner;
+public class GreatestFactorWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = sc.nextInt();
+
+        if (number <= 1) {
+            System.out.println("Please enter an integer greater than 1.");
+            return;
+        }
+
+        int greatestFactor = 1;
+        int counter = number - 1;
+        while (counter >= 1) {
+            if (number % counter == 0) {
+                greatestFactor = counter;
+                break;
+            }
+            counter--;
+        }
+
+        System.out.println("The greatest factor of " + number + " besides itself is: " + greatestFactor);
+    }
+}
