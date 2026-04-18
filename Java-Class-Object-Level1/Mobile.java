5b3de3e0350611dc00dd7c44d2d315b813f4cc09
ANKIT26RAI <anky26roy@gmail.com>
2026-04-18 11:06:54 +0530

    Mobile Characteristics

--- Java-Class-Object-Level1/Mobile.java
+++ Java-Class-Object-Level1/Mobile.java
@@ -3,26 +3,26 @@
 // price. Add a method to display all the details of the phone. The MobilePhone class 
 // uses attributes to store the phone's characteristics. The method is used to retrieve and 
 // display this information for each object.
-class MobilePhone {
-    String brand;
-    String model;
-    double price;
+// class MobilePhone {
+//     String brand;
+//     String model;
+//     double price;
 
-    MobilePhone(String brand, String model, double price) {
-        this.brand = brand;
-        this.model = model;
-        this.price = price;
-    }
+//     MobilePhone(String brand, String model, double price) {
+//         this.brand = brand;
+//         this.model = model;
+//         this.price = price;
+//     }
 
-    void displayDetails() {
-        System.out.println("Brand: " + brand);
-        System.out.println("Model: " + model);
-        System.out.println("Price: $" + price);
-    }
-}
-public class Mobile {
-    public static void main(String[] args) {
-        MobilePhone phone = new MobilePhone("Apple", "iPhone 12", 999.99);
-        phone.displayDetails();
-    }
-}
+//     void displayDetails() {
+//         System.out.println("Brand: " + brand);
+//         System.out.println("Model: " + model);
+//         System.out.println("Price: $" + price);
+//     }
+// }
+// public class Mobile {
+//     public static void main(String[] args) {
+//         MobilePhone phone = new MobilePhone("Apple", "iPhone 12", 999.99);
+//         phone.displayDetails();
+//     }
+// }
