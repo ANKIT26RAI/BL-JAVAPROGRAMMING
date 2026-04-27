875565bcbe76fb824f2caefafc3d6a815e42f4d7
ANKIT26RAI <anky26roy@gmail.com>
2026-04-27 11:02:45 +0530

    Lambda Expressions return and parameters

--- LambdaExpressions/LamExpressions.java
+++ LambdaExpressions/LamExpressions.java
@@ -1,21 +1,62 @@
 //LambdaExpressions are used to define nameless or anonymous methods.
 //If interface  is having only one abstract method it is called functional Interface.
+// @FunctionalInterface
+// interface MyLambda{
+//     public void display();
+    
+// }
+// /*class My implements MyLambda{
+//     public void display(){
+//         System.out.println("HEllo world");
+//     }
+// }*/
+// public class LamExpressions {
+// public static void main(String[] args) {
+
+//     MyLambda m = ()->{System.out.println("Hello World");};
+    
+//     // My m = new My();
+//     m.display();
+// }    
+// }
+
+/*How Lambda Expressions can take Parameter and return result. */
+// @FunctionalInterface
+// interface MyLambda
+// {
+//     public void display(String str);
+// }
+// public class LamExpressions {
+// public static void main(String[] args) {
+//     MyLambda m = (s)->{System.out.println(s);};
+//     m.display("hello world");
+    
+// }
+    
+// }
+
+// @FunctionalInterface
+// interface MyLambda{
+//     public int add(int a,int b);
+// }
+// public class LamExpressions {
+// public static void main(String[] args) {
+//     MyLambda m = (a,b)->{return a+b;};
+//     System.out.println(m.add(20, 30));
+// }
+    
+// }
+
+
 @FunctionalInterface
 interface MyLambda{
-    public void display();
-    
+    public int add(int a,int b);
 }
-/*class My implements MyLambda{
-    public void display(){
-        System.out.println("HEllo world");
-    }
-}*/
 public class LamExpressions {
 public static void main(String[] args) {
-
-    MyLambda m = ()->{System.out.println("Hello World");};
-    
-    // My m = new My();
-    m.display();
-}    
+    MyLambda m = (a,b)->a+b;
+    System.out.println(m.add(20, 30));
 }
+    
+}
+
