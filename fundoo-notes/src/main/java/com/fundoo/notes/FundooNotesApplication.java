3fe89dffde24aecfb47c6def43807d050868d491
ANKIT26RAI <anky26roy@gmail.com>
2026-06-02 11:40:36 +0530

    Prepare backend for deployment with environment variables

--- fundoo-notes/src/main/java/com/fundoo/notes/FundooNotesApplication.java
+++ fundoo-notes/src/main/java/com/fundoo/notes/FundooNotesApplication.java
@@ -1,14 +1,24 @@
 package com.fundoo.notes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class FundooNotesApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            if (System.getProperty(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
+
         SpringApplication.run(FundooNotesApplication.class, args);
     }
 }
